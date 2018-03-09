6d210c18b4fbaa50002e8c3cfbd0a9555c120953
jayasimha <jayasimhajay1994@gmail.com>
2018-03-09 18:41:23 +0530

    Login Application and Json

---
+++ Login.js
@@ -0,0 +1,17 @@
+﻿function validate()
+{
+    var user = document.forms[0];
+    if(document.forms[0].name.value=="jay"&&document.forms[0].password.value==123)
+    {
+        document.getElementById("label1").style.display = 'none';
+        document.getElementById("a1").style.display = 'block';
+        user.name.value = '';
+        user.password.value = '';
+        document.getElementById("b1").style.display = 'none';
+    }
+    else
+    {
+        document.getElementById("a1").style.display = 'none';
+        document.getElementById("label1").style.display = 'block';
+    }
+}
